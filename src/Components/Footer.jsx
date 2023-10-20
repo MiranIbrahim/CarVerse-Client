90a4eb63f2a8bb7feda1732376c2bd842ba437a0
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-10-21 03:16:31 +0600

    footer created completely

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,33 +1,72 @@
-
-
+import logo from "../assets/NavLogo2.png";
+import footerlogo from "../assets/footerlogo.png";
+import { FaFacebook, FaInstagram, FaTwitter, FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-12">
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl mb-4 font-bold">Connect with Us</h2>
-        <p className="mb-8">Stay updated with the latest news and offers!</p>
-        <div className="flex justify-center mb-6">
-          <input
-            type="email"
-            placeholder="Your email"
-            className="px-4 py-2 mr-2 w-64 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-r-md">Subscribe</button>
+    <footer className="bg-gradient-to-r from-gray-100 via-[#bce1ff] to-gray-100">
+      <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div>
+            <img src={logo} className="mr-5 w-3/5 sm:h-9" alt="logo" />
+            <p className="max-w-xs mt-4 text-sm text-gray-600">
+            An unveiling automotive excellence. Discover reviews, insights, and guides for car enthusiasts. Your road to automotive passion starts here.
+            </p>
+            <div className="flex mt-8 space-x-6 text-gray-600">
+              <FaFacebook></FaFacebook>
+              <FaInstagram></FaInstagram>
+              <FaTwitter></FaTwitter>
+              <FaGithub></FaGithub>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4 ml-12">
+            <div>
+              <img src={footerlogo} alt="" className="mt-16" />
+            </div>
+            <div className="ml-36 w-full">
+              <p className="text-[22px] font-bold footer-main">Our Place</p>
+              <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+                <p className="text-[16px]  text-[#646464] font-bold">
+                  221/3-C, Baridhara
+                </p>
+                <p className="text-[16px] text-[#646464] font-medium">
+                  Fazlul Hasan Road,
+                </p>
+                <p className="text-[16px] text-[#646464] font-medium">
+                  Road-16, Store : 27
+                </p>
+                <p className="text-[16px] text-[#646464] font-bold ">
+                  Dhaka-1212
+                </p>
+              </nav>
+            </div>
+            <div className="ml-36 w-full">
+              <p className="text-[22px] font-bold footer-main">Working Hours</p>
+
+              <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+                <p className="text-[16px]  text-[#646464] font-bold">
+                  Monday - Friday:
+                </p>
+                <p className="text-[16px] text-[#646464] font-medium">
+                  7:00am - 21:00pm
+                </p>
+                <p className="text-[16px] text-[#646464] font-bold">
+                  Saturday:
+                </p>
+                <p className="text-[16px] text-[#646464] font-medium">
+                  7:00am - 19:00pm
+                </p>
+                <p className="text-[16px] text-[#646464] font-bold ">
+                  Sunday - Closed
+                </p>
+              </nav>
+            </div>
+          </div>
         </div>
-        <div className="flex justify-center space-x-6">
-          <a href="#" className="text-white hover:text-blue-500">
-            <i className="fab fa-facebook-f text-2xl"></i>
-          </a>
-          <a href="#" className="text-white hover:text-blue-500">
-            <i className="fab fa-twitter text-2xl"></i>
-          </a>
-          <a href="#" className="text-white hover:text-blue-500">
-            <i className="fab fa-instagram text-2xl"></i>
-          </a>
-        </div>
+        <p className="mt-8 text-xs text-gray-800">
+          © 2023 CarVerse all rights reserved
+        </p>
       </div>
     </footer>
   );
-}
-
+};
 export default Footer;
